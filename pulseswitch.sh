1529921932707d445a8a0ea4573479004d92c8df
BoomJacobG <73575421+BoomJacobG@users.noreply.github.com>
2020-10-31 00:41:32 -0400

    Delete pulseswitch.sh

--- pulseswitch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo mv /etc/pulse/daemon2.conf /etc/pulse/daemonswitch.conf
-sudo mv /etc/pulse/daemon.conf /etc/pulse/daemon2.conf
-sudo mv /etc/pulse/daemonswitch.conf /etc/pulse/daemon.conf
-pulseaudio -k 
-sudo cat /etc/pulse/daemon.conf
